decc04ef2d36d6040a602eacb30564dd9c4e8361
Prince Boateng <yb@yb-2.local>
2026-02-23 15:21:49 +0000

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ const App = () => {
 
       <h1>This is a clone of a coinbase</h1>website that is used for buying and selling of cryptocurrency. It is a platform that allows users to buy and sell various cryptocurrencies such as Bitcoin, Ethereum, and Litecoin. The website also provides a wallet for users to store their cryptocurrencies and a dashboard to track their investments. The design of the website is simple and user-friendly, making it easy for users to navigate and use the platform.
       <h1>coin base </h1>
+      <h1>THE WORK BEGINS</h1>
 
 
     </div>
